3b230309aa60e41143332512790f2b22b9596326
happy0611 <101969696+happy0611@users.noreply.github.com>
2022-09-16 18:11:50 +0900

    Add files via upload

--- javascript/CheckEnglishWords.js
+++ javascript/CheckEnglishWords.js
@@ -5,21 +5,24 @@ let exercise_books =
     ["助けが必要ですか？", "符号(？。、)は不要", "do you need help"],
     ["彼らはまだ入院中です","病院 → hospital", "they are still in the hospital"],
     ["彼女は１２時に昼食を食べるのですか？","符号(？。、)は不要","does she eat lunch at twelve"],
-    ["彼は先週末ケーキをつくった","先週 → weekend","he made a cake last weekend."],
-];
+    ["彼は先週末ケーキをつくった","先週 → weekend","he made a cake last weekend"],
+    ["難易度が難しくなるよ！","startを入力して始めてください","start"],
+]
 
 let english_index = 0;
 
+let new_index = english_index % exercise_books.length;
 
-function changedisplayenglish(index) {
+function changedisplayenglish(index, isRight) {
     let question = document.getElementById("DisplayedEnglish"); 
-    //(今日の任務)hint を表示するようにしてください
+    //hintを表示しました
     let hint = document.getElementById("hint");
     hint.textContent = exercise_books[index][1];
-    //(今日の任務)正解 を表示するようにしてください
-    let right = document.getElementById("right");
-    right.textContent = exercise_books[index][2]
-    
+    //間違っている時だけ正解を表示しました
+    if (isRight == false){
+        let right = document.getElementById("right");
+        right.textContent = "間違った問題の正解 " + exercise_books[index][2]
+    }
     question.textContent = exercise_books[index][0]; //textcontentはここに書く
     console.log("change");
 }
@@ -39,13 +42,17 @@ function buttonclick2() {
     let text = document.getElementById("text1").value;
     console.log("get text: " + text);
     //正しいかどうかを判定する
-    if (exercise_books[english_index][2] == text){
+    if (exercise_books[new_index][2] == text){
         result.textContent = "〇";
         english_index += 1;
-        changedisplayenglish(english_index);
+        new_index = english_index % exercise_books.length;
+        changedisplayenglish(new_index, true);
         // console.log("38のindexの今の数字は" + english_index);
     } else {
         result.textContent = "×";
+        // english_index += 1;
+        // new_index = english_index % exercise_books.length;
+        changedisplayenglish(new_index, false);
     }
     //もし正しいであれば丸とid=resultの所で表示する
 }
