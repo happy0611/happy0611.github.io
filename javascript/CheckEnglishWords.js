7835d1a729c6d8dbf3a6401baa9dfae72f6f3cc7
happy0611 <101969696+happy0611@users.noreply.github.com>
2022-09-22 16:51:23 +0900

    Add files via upload

--- javascript/CheckEnglishWords.js
+++ javascript/CheckEnglishWords.js
@@ -1,25 +1,47 @@
 let exercise_books =
     [
-    ["start","startを入力して始めてください","start"],
-    ["こんにちは世界", "こんにちは → hello", "hello world"],
-    ["助けが必要ですか？", "符号(？。、)は不要", "do you need help"],
-    ["彼らはまだ入院中です","病院 → hospital", "they are still in the hospital"],
-    ["彼女は１２時に昼食を食べるのですか？","符号(？。、)は不要","does she eat lunch at twelve"],
-    ["彼は先週末ケーキをつくった","先週 → weekend","he made a cake last weekend"],
-    ["難易度が難しくなるよ！","startを入力して始めてください","start"],
-]
+        ["start", "startを入力して始めてください", "start"],
+        ["こんにちは", "ハローの方", "hello"],
+        ["1月", "", "january"],
+        ["6月", "", "june"],
+        ["10月", "", "october"],
+        ["春", "", "winter"],
+        ["晴れ", "", "sunny"],
+        ["ボール", "", "ball"],
+        ["えんぴつ", "", "pencil"],
+        ["生徒", "", "student"],
+        ["先生", "", "teacher"],
+        ["野球", "", "baseball"],
+        ["サッカー", "", "soccer"],
+        ["英語", "", "english"],
+        ["窓", "", "window"],
+        ["難易度が難しくなるよ！", "startを入力して始めてください", "start"],
+        ["I ( ) a student.", "()を埋めてください", "am"],
+        ["We ( ) are from Canada.", "()を埋めてください", "are"],
+        ["I ( ) soccer", "()を埋めてください", "play"],
+        ["あなたの姉は何歳ですか？", "符号(？。、)は不要", "how old is your sister"]
+        ["私が英語を話すことができます", "できます → can", "i can speak English"],
+        ["助けが必要ですか？", "符号(？。、)は不要", "do you need help"],
+        ["難易度が難しくなるよ！", "startを入力して始めてください", "start"],
+        ["彼らはまだ入院中です", "病院 → hospital", "they are still in the hospital"],
+        ["彼女は１２時に昼食を食べるのですか？", "符号(？。、)は不要", "does she eat lunch at twelve"],
+        ["彼は先週末ケーキをつくった", "先週 → weekend", "he made a cake last weekend"],
+        ["彼女は日本語を話しますか？", "符号(？。、)は不要", "does she speak Japanese"],
+        ["あなたは朝食に何を食べますか？", "符号(？。、)は不要", "what do you have for breakfast"],
+        ["私たちは昨日公園の周りを歩きました", "昨日 → yesterday", "we walked around the park yesterday"],
+    ]
 
 let english_index = 0;
 
 let new_index = english_index % exercise_books.length;
 
 function changedisplayenglish(index, isRight) {
-    let question = document.getElementById("DisplayedEnglish"); 
+    let question = document.getElementById("DisplayedEnglish");
     //hintを表示しました
     let hint = document.getElementById("hint");
     hint.textContent = exercise_books[index][1];
     //間違っている時だけ正解を表示しました
-    if (isRight == false){
+    if (isRight == false) {
         let right = document.getElementById("right");
         right.textContent = "間違った問題の正解 " + exercise_books[index][2]
     }
@@ -42,7 +64,7 @@ function buttonclick2() {
     let text = document.getElementById("text1").value;
     console.log("get text: " + text);
     //正しいかどうかを判定する
-    if (exercise_books[new_index][2] == text){
+    if (exercise_books[new_index][2] == text) {
         result.textContent = "〇";
         english_index += 1;
         new_index = english_index % exercise_books.length;
